d0ec0641612bb765cbdbadef4c69db32313aa439
Zay <Benjaminguillermoburgos@users.noreply.github.com>
2023-04-18 10:19:39 -0400

    subiendo lab 18/04

---
+++ Lab_ej1.py
@@ -0,0 +1,44 @@
+#Ingreso Funciona
+def matriz(filas,columnas,m1):
+    filas=int(input("Ingrese las filas: "))
+    columnas=int(input("Ingrese las columnas: "))
+#for Mostrarfunciona
+    m1=[]
+    for i in range(filas):
+        filas=[]
+        for j in range(columnas):
+            columnas=[]       
+            print(m1[filas][columnas])
+
+#Función SUMA
+    def suma(m1,m2):
+        m1=[]
+        for i in range(filas):
+            filas=[]
+            for j in range(columnas):
+                columnas=[]       
+                print(m1[filas][columnas])
+        m2=[]
+        for i in range(filas):
+            filas=[]
+            for j in range(columnas):
+                columnas=[]       
+                print(m1[filas][columnas])
+        c=m1[filas][columnas]+m2[filas][columnas]
+        print(c)
+#Función Resta
+def resta(m1,m2):
+        m1=[]
+        for i in range(filas):
+            filas=[]
+            for j in range(columnas):
+                columnas=[]       
+                print(m1[filas][columnas])
+        m2=[]
+        for i in range(filas):
+            filas=[]
+            for j in range(columnas):
+                columnas=[]       
+                print(m1[filas][columnas])
+        e=m1[filas][columnas]-m2[filas][columnas]
+        print(e)
