9b6956883fbe80f9236c45e2bb69aaef4eca6865
Zay <Benjaminguillermoburgos@users.noreply.github.com>
2023-04-25 21:43:34 -0400

    change

--- Lab_ej2.py
+++ Lab_ej2.py
@@ -1,47 +1,66 @@
 import  random
 #Metodos
-def matriz_random(filas,columnas):
-    m=[]
+
+filas=int(input("Ingrese Cantidad de filas: "))
+columnas=int(input("Ingrese columnas"))
+
+def crear(filas, columnas, elemento=None):
+    m = []
     for i in range(filas):
-        filas=[]
+        fila = []
         for j in range(columnas):
-            elemento=random.randint(1,5)
-            filas.append(elemento)
-            m.append(filas)
-    return m
-#Resta
-def resta_matriz(m1,m2):
-    matriz_final=[]
-    for i in range(len(m1)):
-        filas=[]
-        for j in range(len(m1[0])):
-            resta=m1[i][j]-m2[i][j]
-            filas.append(resta)
-        matriz_final.append(filas)
-    return matriz_final
+            if elemento is None:
+                elemento=random.randint(1,5)
+            elemento = (hash(f"{i},{j},{elemento}") % 5+1)
+            fila.append(elemento)
+        m.append(fila)
+    return m 
 #Suma
-def suma_matriz(m1,m2):
-    matriz_final=[]
-    for i in range(len(m1)):
-        filas=[]
-        for j in range(len(m1[0])):
-            suma=m1[i][j]+m2[i][j]
-            filas.append(suma)
-        matriz_final.append(filas)
-    return matriz_final
-#Creador
-filas=int(input("Ingrese la cantidad de filas: "))
-columnas=int(input("Ingrese la cantidad de columnas: "))
-print("Creando la primera matriz")
-m1=()
-m2=()
 
+def suma(m,m2):
+    mf = []
+    for i in range(len(m)):
+        fila = []
+        for j in range(len(m[0])):
+            suma = m[i][j] + m2[i][j]
+            fila.append(suma)
+        mf.append(fila)
+    return mf
 #Resta
-matriz_final=resta_matriz(m1,m2)
-print("La resta de las matrices da como resultado la sig.matriz: ")
-print(filas,"/n")
 
-#Suma
-matriz_final=suma_matriz(m1,m2)
-print("La suma de las matrices da como resultado la sig.matriz: ")
-print(filas,"/n")
+def resta(m,m2):
+    mf = []
+    for i in range(len(m)):
+        fila = []
+        for j in range(len(m[0])):
+            resta = m[i][j] - m2[i][j]
+            fila.append(resta)
+        mf.append(fila)
+    return mf
+
+#Resultado
+
+m = crear(filas,columnas, "1")
+print(f"\nMatriz 1:")
+for fila in m:
+    print(fila)
+
+m2 = crear(filas,columnas, "2")
+print(f"\nMatriz 2:")
+for fila in m2:
+    print(fila)
+
+mfinal = suma(m, m2)
+print(f"\nSuma matrices (1+2): ")
+for fila in mfinal:
+    print(mfinal)
+
+m3 = crear(filas,columnas, "3")
+print(f"\nMatriz 3:")
+for fila in m3:
+    print(mfinal)
+
+mfinal = resta(mfinal, m3)
+print(f"\nResta matrices (1+2-3): ")
+for fila in mfinal:
+    print(mfinal)
