65c66751c20a1e37516fc09ab239f73ac338029b
Zay <Benjaminguillermoburgos@users.noreply.github.com>
2023-05-16 10:11:51 -0400

    New Files

---
+++ Lab2_ej1.py
@@ -0,0 +1,44 @@
+#Ingreso Funcion
+def matriz(filas,columnas,a):
+    filas=int(input("Ingrese las filas: "))
+    columnas=int(input("Ingrese las columnas: "))
+#Mostrar funcion
+    a=[]
+    for i in range(filas):
+        filas=[]
+        for j in range(columnas):
+            columnas=[]       
+            print(a[filas][columnas])
+
+#Función SUMA
+    def suma(a,b):
+        a=[]
+        for i in range(filas):
+            filas=[]
+            for j in range(columnas):
+                columnas=[]       
+                print(a[filas][columnas])
+        b=[]
+        for i in range(filas):
+            filas=[]
+            for j in range(columnas):
+                columnas=[]       
+                print(a[filas][columnas])
+        c=a[filas][columnas]+b[filas][columnas]
+        print(c)
+#Función Multiplicacion
+def multiplicacion(a,b):
+        a=[]
+        for i in range(filas):
+            filas=[]
+            for j in range(columnas):
+                columnas=[]       
+                print(a[filas][columnas])
+        b=[]
+        for i in range(filas):
+            filas=[]
+            for j in range(columnas):
+                columnas=[]       
+                print(a[filas][columnas])
+        e=a[filas][columnas]*b[filas][columnas]
+        print(e)
---
+++ Lab2_ej2.dart
@@ -0,0 +1,10 @@
+void main(){
+  List<int>numeros=[3,4,7,9,8,5,1,2,5,7];
+  //Suma
+  int suma=0;
+  for (int numeros in numeros){
+    
+    suma +=numeros;
+  }
+  print(suma);
+}
